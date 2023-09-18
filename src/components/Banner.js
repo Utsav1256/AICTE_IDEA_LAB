9ba8709cfcc462ffedac838adb24b2cdfbbf3d7b
utsav kumar <utsavkumar5462@gmail.com>
2023-09-18 13:37:45 +0530

    add link items

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,15 +1,20 @@
 import JISCE_IMG from "../../static/images/jisce_logo-3.png";
 import JISGroup_IMG from "../../static/images/JIS-Group-logo.png";
 import AICTE_IMG from "../../static/images/aicte_logo_new.png";
+import AICTE_Idea_IMG from "../../static/images/AICTE_Idea.png";
 
 const Banner = () => {
   return (
     <div className="banner_container">
       <div id="img1">
-        <img src={JISCE_IMG} width="200px" height="50px" />
+        <img src={JISCE_IMG} width="220px" height="50px" />
       </div>
       <div id="img3">
-        <img src={AICTE_IMG} width="200px" height="50px" />
+        <img src={AICTE_Idea_IMG} width="70px" height="52px" />
+      </div>
+
+      <div id="img3">
+        <img src={AICTE_IMG} width="220px" height="50px" />
       </div>
       <div id="img2">
         <img src={JISGroup_IMG} width="80px" height="50px" />
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,18 @@ const Header = () => {
         <h1 id="one">AICTE-IDEA_LAB</h1>
         <h1 id="two">JISCE</h1>
       </div>
+      <div className="menu_links">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Event Calender</li>
+          <li>Media</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+      <div className="login">
+        <button>Login / Register</button>
+      </div>
     </div>
   );
 };
