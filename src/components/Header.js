a7af75290434a868513f8ca0b26b2e902423a194
utsav kumar <utsavkumar5462@gmail.com>
2023-09-18 11:11:01 +0530

    add Header logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div id="header">
       <div id="logo">
-        <h1 id="one">AICTE_LAB</h1>
+        <h1 id="one">AICTE-IDEA_LAB</h1>
         <h1 id="two">JISCE</h1>
       </div>
     </div>
