5a6a40ad1322608b9412ab51c9c3cd7263409510
utsav kumar <utsavkumar5462@gmail.com>
2023-09-25 03:19:38 +0530

    add Labs

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,14 @@
 import Welcome from "./Welcome";
 import Gallery from "./Gallery";
+import Labs from "./Labs";
 const Body = () => {
   return (
     <div className="body_container">
       <div id="poster"></div>
       <div id="welcome">
-        <Welcome />
+        {/* <Welcome /> */}
         {/* <Gallery /> */}
+        <Labs />
       </div>
     </div>
   );
--- src/components/Labs.js
+++ src/components/Labs.js
@@ -1,19 +1,42 @@
+import Media from "../media/media";
+import { useState } from "react";
+
 const Labs = () => {
+  const [file, setFile] = useState(null);
+
   return (
     <div id="labs_container">
       <h1>Our Labs</h1>
-      <div id="one">
-        <div id="img1"></div>
-        <div id="img2"></div>
-        <div id="only">
-          <div id="img3"></div>
-          <div id="img4"></div>
-        </div>
+      <div id="media_container">
+        {Media[0].map((file, index) => (
+          <div className="media" key={index} onClick={() => setFile(file)}>
+            {/* {console.log({ file })} */}
+            <img src={file.url} alt="" />
+          </div>
+        ))}
       </div>
-      <div id="two">
-        <div id="img5"></div>
-        <div id="img6"></div>
+
+      <div id="popup_media" style={{ display: file ? "block" : "none" }}>
+        <span onClick={() => setFile(null)}>&times;</span>
+        {<img src={file?.url} />}
       </div>
     </div>
+    // <div id="labs_container">
+    //   <h1>Our Labs</h1>
+    //   <div id="one">
+    //     <div id="imge1"></div>
+    //     <div id="imge2"></div>
+    //     <div id="only">
+    //       <div id="imge3"></div>
+    //       <div id="imge4"></div>
+    //     </div>
+    //   </div>
+    //   <div id="two">
+    //     <div id="img5"></div>
+    //     <div id="img6"></div>
+    //   </div>
+    // </div>
   );
 };
+
+export default Labs;
