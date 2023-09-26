3d40ba6eb288b7c872719f5f3777614b8fcb626e
Shiv <shivshankarshaw2003@gmail.com>
2023-09-26 11:51:59 +0530

    Add files via upload

---
+++ src/components/Location.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Location.css";
+
+function Location(){
+    return(
+        <>
+
+        <h2>Visit Us</h2>
+        <br />
+        <div className="location">
+        <div className="virtual">
+
+            
+            </div>
+        <div className="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3673.756265297744!2d88.4455841149666!3d22.959201784985243!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a027730302f6e25%3A0xe50dfccae21e1fc!2sJIS%20College%20of%20Engineering!5e0!3m2!1sen!2sin!4v1619263040880!5m2!1sen!2sin" width="100%" height="100%" allowfullscreen="" loading="lazy"></iframe>
+            
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Location;
